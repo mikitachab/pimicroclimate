958f6f709586e602fffd69ab57659cb8a1db55a2
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-04-28 17:23:31 +0200

    updated settings

--- pimicroclimate/settings.py
+++ pimicroclimate/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3(xa3jvr-wb4+xl#p2$1qsxaq(pj8bs-00$pb8%!p+gajb5pu)'
+# SECRET_KEY = '3(xa3jvr-wb4+xl#p2$1qsxaq(pj8bs-00$pb8%!p+gajb5pu)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pimicroclimate.herokuapp.com']
 
 
 # Application definition
