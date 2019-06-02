4790ff4ac61909a0d74f2f10d1155c86141956dd
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-06-02 11:03:54 +0200

    added devive id to serializer

--- measurements/serializers.py
+++ measurements/serializers.py
@@ -5,5 +5,5 @@ from .models import Measurement
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ('id', 'datetime', 'temperature', 'humidity', 'light', 'is_loud', 'is_valid')
+        fields = ('id', 'datetime', 'temperature', 'humidity', 'light', 'is_loud', 'is_valid', 'device_id')
         ordering = ['-id']
