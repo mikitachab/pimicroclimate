be40e575d53ccd08e68fd8b127f40a333cf95f81
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-06-09 11:42:17 +0200

    fix typo

--- pimicroclimate/view.py
+++ pimicroclimate/view.py
@@ -2,11 +2,12 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     context = {
-            "title" : "Welcome on PiMicolimate",
-            "data_table" : "/data/table",
-            "data_plot" : "/data/plot",
-            "compare" : "/data/compare"
-        }
+        "title": "Welcome on PiMicroclimate",
+        "data_table": "/data/table",
+        "data_plot": "/data/plot",
+        "compare": "/data/compare"
+    }
     return render(request, "index.html", context)
